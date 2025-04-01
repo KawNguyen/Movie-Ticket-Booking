89f48c746d590f799a0d22dac293167688acd185
KawNguyen <khoanguyencool12@gmail.com>
2025-04-02 00:30:51 +0700

    update

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,20 +1,34 @@
+import axios from 'axios';
+
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY || process.env.TMDB_API_KEY;
 
 export async function fetchTmdbData(endpoint: string) {
-  const res = await fetch(`${BASE_URL}/${endpoint}?api_key=${API_KEY}`);
-  if (!res.ok) throw new Error("Failed to fetch data");
-  return res.json();
+  const { data } = await axios.get(`${BASE_URL}/${endpoint}`, {
+    params: {
+      api_key: API_KEY
+    }
+  });
+  return data;
 }
 
 export async function searchTmdbData(endpoint: string) {
-  const res = await fetch(`${BASE_URL}/${endpoint}&api_key=${API_KEY}`);
-  if (!res.ok) throw new Error("Failed to fetch data");
-  return res.json();
+  const { data } = await axios.get(`${BASE_URL}/${endpoint}`, {
+    params: {
+      api_key: API_KEY
+    }
+  });
+  return data;
 }
 
 export async function fetchTmdbDetail(movieid: string) {
-  const res = await fetch(`${BASE_URL}/movie/${movieid}?api_key=${API_KEY}&language=en-EN&append_to_response=credits,videos`);
-  if (!res.ok) throw new Error("Failed to fetch data");
-  return res.json();
+  const { data } = await axios.get(`${BASE_URL}/movie/${movieid}`, {
+    params: {
+      api_key: API_KEY,
+      language: 'en-EN',
+      append_to_response: 'credits,videos'
+    }
+  });
+  return data;
 }
+
