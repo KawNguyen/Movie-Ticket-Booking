73e8ccbdf942e87698af490a91f3076a3216c2a1
KawNguyen <khoanguyencool12@gmail.com>
2025-04-09 14:25:09 +0700

    update

--- app/(root)/payment-success/page.tsx
+++ app/(root)/payment-success/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Suspense, use } from "react";
+import { Suspense } from "react";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
